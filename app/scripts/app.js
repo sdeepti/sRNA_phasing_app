ceed1668497d5ccc26100657934f528a8e219e1c
JasonRafeMiller <jmiller@jcvi.org>
2015-03-03 08:53:09 -0500

    The app calls the at_srna API. For this trivial test, the parameters are hard coded and the result JSON object is merely dumped to the javascript console.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,43 +3,82 @@
 
   console.log('This is the Araport science app for Arabidopsis thaliana small RNA data.');
 
-  /* data-app-name is an attribute of a div in app.html */
-  /* app.html is included by a div in index.html */
+  /** 
+   * data-app-name is an attribute of a div in app.html 
+   * app.html is included by a div in index.html 
+   */
   var appContext = $('[data-app-name="table-app-1"]');
 
-  /* Create an anonymous function and register it to fire when AgaveAPI is ready. */
-  /* Our science app functionality belongs inside this function. */
+    /**
+     * Create an anonymous function and register it to fire when AgaveAPI is ready. 
+     * Our science app functionality belongs inside this function. 
+     */
   window.addEventListener('Agave::ready', function() {
       var Agave, interactive, info;
       var htmlString;
 
       /* This object has useful functions for interacting with Araport server components. */
       Agave = window.Agave;
+      /** 
+       * This is redundant because this function was called on Agave ready! 
+       * However this is demonstrative of an Agave call. 
+       * The getStatus() call takes three parameters: args, callback, error. 
+       * We pass an empty opbject, an anonymous function, and omit error. 
+       */
+      Agave.api.adama.getStatus({}, function(resp) {
+	  if (resp.obj.status === 'success') {
+	      console.log('Agave status is good!');
+	  } else {
+	      console.log('Agave status is NOT good!');
+	  }
+      }); // end of getStatus 
 
-      /* Use the jQuery .html() setter/getter function to CHANGE content. */
-      /* Alter the HTML title to show we are running. */
-      /* Add a div to hold the interactive application content. */
-      /* Add a div to hold the provenance information. */
-      /* Each div could have a unique id. */
-      /* Each div could have one or more classes for sytling. */
+      /**
+       * Use the jQuery .html() setter/getter function to CHANGE content. 
+       * Alter the HTML title to show we are running. 
+       * Add a div to hold the interactive application content. 
+       * Add a div to hold the provenance information. 
+       * Each div could have a unique id. 
+       * Each div could have one or more classes for sytling. 
+       */
       htmlString='<h2><em>Arabidopsis thaliana</em> small RNA</h2>'+
 	  '<div class="interactive"></div>'+
 	  '<hr><div class="provenance-info"></div><br>';
       appContext.html(htmlString);
       
-      /* Search the appContenxt div for elements with attribute interactive. */
-      /* Note we added this element in a previous line. */
+      /**
+       * Search the appContenxt div for elements with attribute interactive. 
+       * Note we added this element in a previous line. 
+       */
       interactive = $('.interactive', appContext);
       interactive.append('<p>This is your interactive app!</p>');
 
-      /* Search the appContenxt div for elements with attribute provenance-info. */
-      /* Adjust the provenance info at the bottom of the screen */
+      /**
+       * Search the appContenxt div for elements with attribute provenance-info. 
+       * Adjust the provenance info at the bottom of the screen 
+       */
       info = $('.provenance-info', appContext);
       /* The bootstrap.css (in use at Araport) recognizses class "text-center" and centers it. */
       info.addClass('text-center');
-      /* For now, hard code some provenance info. */
-      /* Later, get provenance from a Meyers Lab web service. */
+      /**
+       * For now, hard code some provenance info. 
+       * Later, get provenance from a Meyers Lab web service. 
+       */
       info.append('<p>Visit <a href="http://www.meyerslab.org/data/">Meyers Lab Data</a> for more information!</p>');
+
+      /**
+       * Execute a search.
+       */
+      Agave.api.adama.search(
+          {'namespace': 'at_srna', 
+	   'service': 'at_srna_v0.1', 
+	   'queryParams': {'chr': 1, 'beg': 9000, 'end': 9400}},
+          function(search) {
+	      console.log('Here is the Agave search result object!');              
+	      console.log(search);
+          }
+      );
+        
   });
 
 })(window, jQuery);
